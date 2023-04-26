5d1e0f9efe0a7b79df8e71027a5b3da4a6bfc517
Bruno Silva Barbosa <brunobarbosa@dgtbr.com.br>
2023-04-26 16:40:52 -0400

    ola

---
+++ src/main/java/br/com/yuuorkillua/testeJavaBackend/TesteJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.yuuorkillua.testeJavaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yuuorkillua/testeJavaBackend/TesteJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yuuorkillua.testeJavaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
