3a7103a6f61bacde05820d758b817d21b81c0aa3
Bruno Barbosa <barbosa.c3.bb@gmail.com>
2023-05-03 00:09:10 -0700

    Foi

--- src/main/java/br/com/yuuorkillua/testeJavaBackend/TesteJavaBackendApplication.java
+++ src/main/java/br/com/yuuorkillua/testeJavaBackend/TesteJavaBackendApplication.java
@@ -1,5 +1,5 @@
 package br.com.yuuorkillua.testeJavaBackend;
-jhdfgfbeduidw
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
