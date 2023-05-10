e7b578d9d2a26fe6bab95860fb50e177eebf55d0
Bruno Barbosa <barbosa.c3.bb@gmail.com>
2023-05-10 16:34:55 -0400

    sla

--- src/main/java/br/com/yuuorkillua/testeJavaBackend/Componentes/Moto.java
+++ src/main/java/br/com/yuuorkillua/testeJavaBackend/Componentes/Moto.java
@@ -3,10 +3,8 @@ package br.com.yuuorkillua.testeJavaBackend.Componentes;
 import jakarta.validation.constraints.NotBlank;
 
 public class Moto extends Veiculo{
-
     public Moto(@NotBlank String nome, @NotBlank String marca, int ano) {
         super(nome, marca, ano);
-
     }
     
 }
